77a8a29b30c714196e03633352ceac800b9cd1c9
georgy <ganus1111@gmail.com>
2022-09-13 23:02:22 +0300

    add task solution

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        int res;
+
+        if( (year % 4 == 0) && (year % 100 != 0) || year % 400 == 0 ){
+            res = 1;
+        }else {
+            res = 0;
+        }
+        switch (res){
+            case (1):
+                System.out.println("leap");
+                break;
+            case (0):
+                System.out.println("not leap");
+                break;
+        }
     }
 }
